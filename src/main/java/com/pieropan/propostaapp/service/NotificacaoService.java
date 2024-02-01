2695102e7828c847e89f2ef0d5df1bc0abbde4cb
mamapisouza <Pi204326@>
2024-01-31 21:47:00 -0300

    novas implementações

---
+++ src/main/java/com/pieropan/propostaapp/service/NotificacaoService.java
@@ -0,0 +1,17 @@
+package com.pieropan.propostaapp.service;
+
+import com.pieropan.propostaapp.dto.PropostaResponseDto;
+import lombok.AllArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class NotificacaoService {
+
+    private RabbitTemplate rabbitTemplate;
+
+    public void notificar(PropostaResponseDto proposta, String exchange) {
+        rabbitTemplate.convertAndSend(exchange, "", proposta);
+    }
+}
