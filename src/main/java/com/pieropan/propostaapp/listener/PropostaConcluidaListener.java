79544671433bdbd1bc575ec64eb8f5c5d1c531c8
mamapisouza <Pi204326@>
2024-02-05 11:58:05 -0300

    ws

--- src/main/java/com/pieropan/propostaapp/listener/PropostaConcluidaListener.java
+++ src/main/java/com/pieropan/propostaapp/listener/PropostaConcluidaListener.java
@@ -1,19 +1,26 @@
 package com.pieropan.propostaapp.listener;
 
+import com.pieropan.propostaapp.dto.PropostaResponseDto;
 import com.pieropan.propostaapp.entity.Proposta;
+import com.pieropan.propostaapp.mapper.PropostaMapper;
 import com.pieropan.propostaapp.repository.PropostaRepository;
+import com.pieropan.propostaapp.service.WebSocketService;
+import lombok.AllArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+@AllArgsConstructor
 @Component
 public class PropostaConcluidaListener {
 
-    @Autowired
     private PropostaRepository propostaRepository;
+
+    private WebSocketService webSocketService;
 
     @RabbitListener(queues = "${rabbitmq.queue.proposta.concluida}")
     public void propostaConcluida(Proposta proposta) {
         propostaRepository.save(proposta);
+        PropostaResponseDto responseDto = PropostaMapper.INSTANCE.convertEntityToDto(proposta);
+        webSocketService.notificar(responseDto);
     }
 }
